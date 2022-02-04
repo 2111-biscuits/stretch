c1aafaab5e43accc2759b2fef650d3e1438cc10a
Cara Dodge <shxpxshxftxr@gmail.com>
2022-02-04 17:29:17 -0500

    landing page css

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -3,31 +3,31 @@ import { Link, useHistory } from "react-router-dom";
 
 class LandingPage extends React.Component {
   render() {
-    console.log(this.props);
     return (
       <div id="landing">
-        <div id="header"></div>
+
         <div id="top">
-          <h3>Artist Name</h3>
-          <h3>Artist Name</h3>
-          <h3>Artist Name</h3>
-          <h3>Artist Name</h3>
-          <h3>Artist Name</h3>
-          <h3>Artist Name</h3>
+          <h1>Galleria</h1>
         </div>
-        <div id="bottom">
-          <h1>Name of Exhibit</h1>
-          <Link to="/gallery">
-            <button id="enter-button">Enter</button>
+
+        <div>
+        <Link to="/gallery">
+            <button id="enter-button">ENTER</button>
           </Link>
-          <div id="dates">
-            <h4>JAN 28 - MAR 14 2022</h4>
           </div>
+
+        <div id="bottom">
+          <h4 id="dates">JAN 26 - FEB 18 2022</h4>
+
+
+
+
+          <h4 id="artists">
+            Featuring the works of Lucy Blaggs, Nicole DeSantis, Yooboo Park,
+            Cara Dodge, and German Caceres
+          </h4>
         </div>
-        <div id="footer">
-          <div id="left"></div>
-          <div id="right"></div>
-        </div>
+
       </div>
     );
   }
