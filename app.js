4d0795919f6d2b04486602abd9401d69e949db52
Cara Dodge <shxpxshxftxr@gmail.com>
2022-02-06 14:59:48 -0500

    port on main

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const http = require("http").Server(app);
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "./public")));
 
@@ -9,6 +10,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-http.listen(8080, function () {
-  console.log("listening on *:8080 - origin set");
+http.listen(PORT, function () {
+  console.log("listening on *:HERE - origin set");
 });
