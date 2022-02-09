f959b046386593f7a7fca0a230ea02b83908e411
Cara Dodge <shxpxshxftxr@gmail.com>
2022-02-09 16:48:26 -0500

    backend bones for art all and art single react pages

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const http = require("http").Server(app);
 const PORT = process.env.PORT || 8080;
+const db  = require("./db/database")
 
 app.use(express.static(path.join(__dirname, "./public")));
 
@@ -10,6 +11,12 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-http.listen(PORT, function () {
+db.sync()
+.then(() => {
+  console.log("db synced")
+  http.listen(PORT, function () {
   console.log("listening on *:here - origin set");
-});
+  })
+})
+
+
