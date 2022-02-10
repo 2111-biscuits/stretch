6cc78c2c82762e394149be357307c35f834b541d
Cara Dodge <shxpxshxftxr@gmail.com>
2022-02-10 12:41:13 -0500

    fully debugged

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import Root from "./components/Root";
 import store from './store'
+import { Provider } from "react-redux"
 // import history from './history'
 
 ReactDOM.render(
