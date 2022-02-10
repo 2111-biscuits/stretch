9eb839dcaeaa45e4226a62a9d9c6aa15541788b8
Cara Dodge <shxpxshxftxr@gmail.com>
2022-02-10 12:37:18 -0500

    debugged

--- client/index.js
+++ client/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import Root from "./components/Root";
 import store from './store'
-import history from './history'
+// import history from './history'
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
+    <Router >
       <Root />
     </Router>
   </Provider>,
