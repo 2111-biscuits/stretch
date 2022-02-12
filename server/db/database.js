f74cc9fa7ade79dccf5eec4194f76513502dce02
NicoDesa13 <91211213+NicoDesa13@users.noreply.github.com>
2022-02-12 17:39:43 -0500

    Update database.js

--- server/db/database.js
+++ server/db/database.js
@@ -4,9 +4,24 @@ const pkg = require("../../package.json");
 const dbName = process.env.NODE_ENV === "test" ? `${pkg.name}-test` : pkg.name;
 console.log(`Opening database connection to ${dbName}`);
 
-const db = new Sequelize(`postgres://localhost:5432/${dbName}`, {
-  logging: false
-});
+const config = {
+  logging: false,
+  //   username: 'postgres'
+};
+
+//https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
+if (process.env.DATABASE_URL) {
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
+
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
+  config
+);
 
 module.exports = db;
 
